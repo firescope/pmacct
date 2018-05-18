8cb4dd84eedf9615f2afe4e0fcd10f898c006fac
hiep-huynh <hhuynh@firescope.com>
2018-05-18 15:01:38 +0000

    Fix flow_url build

--- build-all.sh
+++ build-all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -ex
 yum -y install libpcap-devel jansson-devel openssl-devel git gcc libtool make cmake tcpdump
 cp -pfr dependencies/lib/* /usr/local/lib
 cp -pfr dependencies/include/* /usr/local/include
@@ -8,8 +9,8 @@ export RABBITMQ_CFLAGS=-I/usr/local/include
 ./configure --enable-rabbitmq --enable-jansson
 make -j4
 make install
+
+# Build url sniffer
+mkdir -p /firescope/system_config/pmacct/classifiers 
 cd src/classifiers
-#gcc -o check_endianess check_endianess.c
-#gcc -shared -fPIC `./check_endianess` -o flow_url.so flow_url.c -ldl
 make install
-#cp ./flow_url.so /firescope/system_config/pmacct/classifiers
